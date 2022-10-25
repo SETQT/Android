f282dd76438a700fc7b6fe52652a8d691fefb5bc
20120376 <85086936+that161@users.noreply.github.com>
2022-10-26 00:06:24 +0700

    update dashboard

--- app/src/main/java/com/example/androidproject08/CustomProductLabelAdapter.java
+++ app/src/main/java/com/example/androidproject08/CustomProductLabelAdapter.java
@@ -1,6 +1,7 @@
 package com.example.androidproject08;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +14,16 @@ public class CustomProductLabelAdapter extends BaseAdapter {
     int logos[];
     String names[];
     String costs[];
+    String costs_sale[];
+    String percents_sale[];
     LayoutInflater inflter;
-    public CustomProductLabelAdapter(Context applicationContext, int[] logos, String[] names, String[] costs) {
+    public CustomProductLabelAdapter(Context applicationContext, int[] logos, String[] names, String[] costs, String[] costs_sale, String[] percents_sale) {
         this.context = applicationContext;
         this.logos = logos;
         this.names = names;
         this.costs = costs;
+        this.costs_sale = costs_sale;
+        this.percents_sale = percents_sale;
         inflter = (LayoutInflater.from(applicationContext));
     }
     @Override
@@ -39,9 +44,13 @@ public class CustomProductLabelAdapter extends BaseAdapter {
         ImageView picture = (ImageView) view.findViewById(R.id.picture_product); // get the reference of ImageView
         TextView name = (TextView) view.findViewById(R.id.name_product);
         TextView cost = (TextView) view.findViewById(R.id.cost_product);
+        TextView cost_sale = (TextView) view.findViewById(R.id.cost_sale_product);
+        TextView percent_sale = (TextView) view.findViewById(R.id.percent_sale_product);
         picture.setImageResource(logos[i]); // set logo images
         name.setText(names[i]);
         cost.setText(costs[i]);
+        cost_sale.setText(costs_sale[i]);
+        percent_sale.setText(percents_sale[i]);
         return view;
     }
 }
--- app/src/main/java/com/example/androidproject08/MainActivity.java
+++ app/src/main/java/com/example/androidproject08/MainActivity.java
@@ -9,9 +9,12 @@ import android.widget.GridView;
 
 public class MainActivity extends Activity {
     GridView gridview;
-    int logos[] = {R.drawable.mono1, R.drawable.ao1, R.drawable.ao2, R.drawable.mono1, R.drawable.ao1, R.drawable.ao2, R.drawable.mono1, R.drawable.ao1};
-    String names[] = {"Áo khoác cực chất","Waiting for you", "Áo khoác cực chất","Waiting for you", "Áo khoác cực chất","Waiting for you", "Áo khoác cực chất","Waiting for you"};
-    String costs[] = {"đ100.000", "đ500.000", "đ100.000", "đ500.000", "đ100.000", "đ500.000", "đ100.000", "đ500.000"};
+    int logos[] = {R.drawable.mono1, R.drawable.ao1, R.drawable.ao2, R.drawable.mono1, R.drawable.ao1, R.drawable.ao2, R.drawable.mono1, R.drawable.ao1, R.drawable.mono1, R.drawable.ao2};
+    String names[] = {"Áo khoác cực chất","Waiting for you", "Áo khoác cực chất","Waiting for you", "Áo khoác cực chất","Waiting for you", "Áo khoác cực chất","Waiting for you",
+            "Áo khoác cực chất","Waiting for you"};
+    String costs[] = {"đ100.000", "đ500.000", "đ100.000", "đ500.000", "đ100.000", "đ500.000", "đ100.000", "đ500.000", "đ100.000", "đ500.000"};
+    String costs_sale[] = {"đ80.000", "đ400.000", "đ80.000", "đ400.000", "đ80.000", "đ400.000", "đ80.000", "đ400.000", "đ80.000", "đ400.000"};
+    String percent_sale[] = {"-20%", "-20%", "-20%", "-20%", "-20%", "-20%", "-20%", "-20%", "-20%", "-20%"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.dashboard);
         gridview = (GridView) findViewById(R.id.gridview);
 
-        CustomProductLabelAdapter customAdapter = new CustomProductLabelAdapter(getApplicationContext(), logos, names, costs);
+        CustomProductLabelAdapter customAdapter = new CustomProductLabelAdapter(getApplicationContext(), logos, names, costs, costs_sale, percent_sale);
         gridview.setAdapter(customAdapter);
+
     }
 }
