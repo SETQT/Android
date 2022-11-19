2f86797247afc97a59484143f5303d8fe1227d62
20120376 <85086936+that161@users.noreply.github.com>
2022-11-20 01:31:41 +0700

    update dashboard

--- app/src/main/java/com/example/androidproject08/DashboardFragmentFirst.java
+++ app/src/main/java/com/example/androidproject08/DashboardFragmentFirst.java
@@ -1,23 +1,34 @@
 package com.example.androidproject08;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class DashboardFragmentFirst extends Fragment {
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class DashboardFragmentFirst extends Fragment{
 
     activity_dashboard main;
-    Button dashboard_option_tatca, dashboard_option_aokhoac, dashboard_option_mu, dashboard_option_quan, dashboard_option_ao;
-
+    ArrayList<ListViewOptionDashboard> list = new ArrayList<ListViewOptionDashboard>();
+    ArrayList<String> text = new ArrayList<>();
 
     // convenient constructor(accept arguments, copy them to a bundle, binds bundle to fragment)
     public static DashboardFragmentFirst newInstance(String strArg) {
@@ -41,103 +52,25 @@ public class DashboardFragmentFirst extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         LinearLayout layout_first = (LinearLayout) inflater.inflate(R.layout.custom_dashboard_layout_fragment_first, null);
 
-        dashboard_option_tatca  = (Button) layout_first.findViewById(R.id.dashboard_option_tatca);
-        dashboard_option_aokhoac  = (Button) layout_first.findViewById(R.id.dashboard_option_aokhoac);
-        dashboard_option_mu  = (Button) layout_first.findViewById(R.id.dashboard_option_mu);
-        dashboard_option_quan  = (Button) layout_first.findViewById(R.id.dashboard_option_quan);
-        dashboard_option_ao  = (Button) layout_first.findViewById(R.id.dashboard_option_ao);
-
-        dashboard_option_tatca.setTextAppearance(getActivity(), R.style.setTextAfterClickDashBoard);
-        dashboard_option_tatca.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F9A826")));
-
-        dashboard_option_tatca.setOnClickListener(new View.OnClickListener() {
+        RecyclerView recyclerView = layout_first.findViewById(R.id.dashboard_listview);
+        ListTypeOptionDashboard mAdapter = new ListTypeOptionDashboard(list, new IClickItemListener() {
             @Override
-            public void onClick(View view) {
-                dashboard_option_tatca.setTextAppearance(getActivity(), R.style.setTextAfterClickDashBoard);
-                dashboard_option_tatca.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F9A826")));
-                dashboard_option_aokhoac.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_aokhoac.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_mu.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_mu.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_quan.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_quan.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_ao.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_ao.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-
-                String dataSend = "Tat ca";
+            public void onClickItem(ListViewOptionDashboard l) {
+                String dataSend = l.getText();
                 main.onMsgFromFragToMain("BLUE-FRAG", dataSend);
-            }});
+            }
+        });
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(main.getApplicationContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
 
-        dashboard_option_aokhoac.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dashboard_option_tatca.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_tatca.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_aokhoac.setTextAppearance(getActivity(), R.style.setTextAfterClickDashBoard);
-                dashboard_option_aokhoac.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F9A826")));
-                dashboard_option_mu.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_mu.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_quan.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_quan.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_ao.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_ao.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
+        text.add("Tất cả");text.add("Áo khoác");text.add("Quần");text.add("Áo"); text.add("Mũ");
 
-                String dataSend = "Ao khoac";
-                main.onMsgFromFragToMain("BLUE-FRAG", dataSend);
-            }});
-
-        dashboard_option_mu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dashboard_option_tatca.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_tatca.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_aokhoac.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_aokhoac.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_mu.setTextAppearance(getActivity(), R.style.setTextAfterClickDashBoard);
-                dashboard_option_mu.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F9A826")));
-                dashboard_option_quan.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_quan.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_ao.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_ao.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-
-                String dataSend = "Mu";
-                main.onMsgFromFragToMain("BLUE-FRAG", dataSend);
-            }});
-
-        dashboard_option_quan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dashboard_option_tatca.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_tatca.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_aokhoac.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_aokhoac.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_mu.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_mu.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_quan.setTextAppearance(getActivity(), R.style.setTextAfterClickDashBoard);
-                dashboard_option_quan.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F9A826")));
-                dashboard_option_ao.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_ao.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                String dataSend = "Quan";
-                main.onMsgFromFragToMain("BLUE-FRAG", dataSend);
-            }});
-
-        dashboard_option_ao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dashboard_option_tatca.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_tatca.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_aokhoac.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_aokhoac.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_mu.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_mu.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_quan.setTextAppearance(getActivity(), R.style.setTextNotClickDashBoard);
-                dashboard_option_quan.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                dashboard_option_ao.setTextAppearance(getActivity(), R.style.setTextAfterClickDashBoard);
-                dashboard_option_ao.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F9A826")));
-
-                String dataSend = "Ao";
-                main.onMsgFromFragToMain("BLUE-FRAG", dataSend);
-            }});
+        for (int i = 0; i < text.size(); i++) {
+            list.add(new ListViewOptionDashboard(text.get(i)));
+        }
 
         return layout_first;
     }// onCreateView
@@ -147,5 +80,7 @@ public class DashboardFragmentFirst extends Fragment {
         main.onMsgFromFragToMain("BLUE-FRAG", dataSend);
 
     }
+
+
 }// class
 
