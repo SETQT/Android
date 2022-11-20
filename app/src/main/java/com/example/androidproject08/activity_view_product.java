d5071a5bf1e817a8aa133fa024f2db6bd7ab15e1
hoduybao <82367666+hoduybao@users.noreply.github.com>
2022-11-20 15:41:00 +0700

    update UI View_Product

--- app/src/main/java/com/example/androidproject08/activity_view_product.java
+++ app/src/main/java/com/example/androidproject08/activity_view_product.java
@@ -1,19 +1,24 @@
-package com.example.androidproject08;
+package com.example.doan;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 import java.util.ArrayList;
 
-public class activity_view_product extends AppCompatActivity {
-    private ArrayList<Type_View_Product> listType = new ArrayList<Type_View_Product>();
-    private ListTypeProductAdapter mAdapter;
+public class activity_view_product extends Activity {
+    private ArrayList<Type_View_Product> listType_color = new ArrayList<Type_View_Product>();
+    private ArrayList<Type_View_Product> listType_size = new ArrayList<Type_View_Product>();
+
+
+    private ListTypeProductAdapter mAdapter_color;
+    private ListTypeProductAdapter mAdapter_size;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,26 +26,51 @@ public class activity_view_product extends AppCompatActivity {
         // getting reference of  ExpandableTextView
         ExpandableTextView expTv = (ExpandableTextView) findViewById(R.id.expand_text_view).findViewById(R.id.expand_text_view);
 
-        // calling setText on the ExpandableTextView so that
-        // text content will be  displayed to the user
-
+// calling setText on the ExpandableTextView so that
+// text content will be  displayed to the user
         expTv.setText(getString(R.string.expandable_text));
-        RecyclerView recyclerView = findViewById(R.id.recyclerView_view_product);
-        mAdapter = new ListTypeProductAdapter(listType);
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(mAdapter);
-        prepareMovieData();
+
+
+        RecyclerView recyclerView_color = findViewById(R.id.recyclerView_color_view_product);
+        mAdapter_color = new ListTypeProductAdapter(listType_color);
+        LinearLayoutManager mLayoutManager_color = new LinearLayoutManager(getApplicationContext());
+        mLayoutManager_color.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView_color.setLayoutManager(mLayoutManager_color);
+        recyclerView_color.setItemAnimator(new DefaultItemAnimator());
+        recyclerView_color.setAdapter(mAdapter_color);
+        prepareTypeColor();
+
+        mAdapter_size = new ListTypeProductAdapter(listType_size);
+        RecyclerView recyclerView_size = findViewById(R.id.recyclerView_size_view_product);
+        LinearLayoutManager mLayoutManager_size = new LinearLayoutManager(getApplicationContext());
+        mLayoutManager_size.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView_size.setLayoutManager(mLayoutManager_size);
+        recyclerView_size.setItemAnimator(new DefaultItemAnimator());
+        recyclerView_size.setAdapter(mAdapter_size);
+        prepareTypeSize();
+
+
     }
-    private void prepareMovieData() {
+
+    private void prepareTypeColor() {
         Type_View_Product type = new Type_View_Product("Đen");
-        listType.add(type);
+        listType_color.add(type);
         type = new Type_View_Product("Đỏ");
-        listType.add(type);
+        listType_color.add(type);
         type = new Type_View_Product("Trắng");
-        listType.add(type);
-        mAdapter.notifyDataSetChanged();
+        listType_color.add(type);
+        mAdapter_color.notifyDataSetChanged();
+
+    }
+
+    private void prepareTypeSize() {
+        Type_View_Product type = new Type_View_Product("Đen");
+        type = new Type_View_Product("M");
+        listType_size.add(type);
+        type = new Type_View_Product("L");
+        listType_size.add(type);
+        type = new Type_View_Product("XL");
+        listType_size.add(type);
+        mAdapter_size.notifyDataSetChanged();
     }
 }
