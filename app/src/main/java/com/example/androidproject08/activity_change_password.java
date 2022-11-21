0cd96edf0e469e8fe761da4d7ff37d6f3ba97cb9
hoduybao <82367666+hoduybao@users.noreply.github.com>
2022-11-21 16:29:37 +0700

    add UI_change_password

---
+++ app/src/main/java/com/example/androidproject08/activity_change_password.java
@@ -0,0 +1,13 @@
+package com.example.androidproject08;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_change_password extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
