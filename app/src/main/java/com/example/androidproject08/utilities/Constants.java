83e56bb04ea79c74c432f19f030d9e4788de5d27
20120473 <76527212+duongminhhieu@users.noreply.github.com>
2022-12-06 16:50:07 +0700

    Recent Conversations for user

--- app/src/main/java/com/example/androidproject08/utilities/Constants.java
+++ app/src/main/java/com/example/androidproject08/utilities/Constants.java
@@ -3,6 +3,7 @@ package com.example.androidproject08.utilities;
 public class Constants {
     public static final String KEY_COLLECTION_ADMIN ="admin";
     public static final String KEY_ADMIN_NAME = "name";
+    public static final String KEY_USER_NAME = "name";
     public static final String KEY_PASSWORD = "password";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
     public static final String KEY_ADMIN_ID = "adminID";
@@ -16,4 +17,10 @@ public class Constants {
     public static final String KEY_RECEIVER_ID = "receiverId";
     public static final String KEY_MESSAGE = "message";
     public static final String KEY_TIMESTAMP = "timestamp";
+    public static final String KEY_COLLECTION_CONVERSATIONS = "conversations";
+    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_RECEIVER_NAME = "receiverName";
+    public static final String KEY_SENDER_IMAGE = "senderImage";
+    public static final String KEY_RECEIVER_IMAGE = "receiverImage";
+    public static final String KEY_LAST_MESSAGE = "lastMessage";
 }
