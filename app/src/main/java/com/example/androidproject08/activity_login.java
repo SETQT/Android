36773f0a3e93126080339ba00d57f4d7b3df4170
20120471 <truongvanhao159@gmail.com>
2022-12-18 16:10:39 +0700

    [18/12/22] [hao] done login with google https://duongminhhieu.atlassian.net/browse/A2G-101?atlOrigin=eyJpIjoiNjhiYTdhMTczZDc0NGE0MWEwMTczMjNiMjIyMmJkZTMiLCJwIjoiaiJ9

--- app/src/main/java/com/example/androidproject08/activity_login.java
+++ app/src/main/java/com/example/androidproject08/activity_login.java
@@ -2,17 +2,14 @@ package com.example.androidproject08;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -21,19 +18,32 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
-public class activity_login extends Activity {
+public class activity_login extends Activity implements View.OnClickListener {
     View btn_register;
     EditText edittext_tk, edittext_mk;
-    Button btn_login;
+    Button btn_login, btn_google;
+
+    // google
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     // facebook
     private CallbackManager mCallbackManager;
@@ -41,30 +51,38 @@ public class activity_login extends Activity {
     // kết nối sqlite
     SQLiteDatabase sqlite;
 
+    // kết nối firestore
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference usersRef = db.collection("users");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.example.androidproject08",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        }
-        catch (PackageManager.NameNotFoundException e) {
-        }
-        catch (NoSuchAlgorithmException e) {
-        }
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.androidproject08",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//        } catch (NoSuchAlgorithmException e) {
+//        }
 
         btn_register = (View) findViewById(R.id.rectangle_4);
+        btn_register.setOnClickListener(this);
         btn_login = (Button) findViewById(R.id.btn_dangnhap);
+        btn_login.setOnClickListener(this);
         edittext_tk = (EditText) findViewById(R.id.edittext_tk);
         edittext_mk = (EditText) findViewById(R.id.edittext_mk); // mật khẩu người dùng
+        btn_google = (Button) findViewById(R.id.btn_google);
+        btn_google.setOnClickListener(this);
 
         // login bằng facebook
         mCallbackManager = CallbackManager.Factory.create();
@@ -87,69 +105,66 @@ public class activity_login extends Activity {
                         Toast.makeText(getApplicationContext(), "Đăng nhập bằng facebook thất bại!", Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tk = edittext_tk.getText().toString();// tài khoản người dùng
-                String mk = edittext_mk.getText().toString();// mật khẩu người dùng
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == btn_register.getId()) {
+            // chuyển sang activity đăng ký
+            Intent moveActivity = new Intent(activity_login.this, activity_register.class);
+            startActivity(moveActivity);
+        }
 
-                // kiem tra empty
-                if (tk.equals("") || mk.equals("")) {
-                    Toast.makeText(activity_login.this, "Vui lòng điền đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+        if (view.getId() == btn_login.getId()) {
+            String tk = edittext_tk.getText().toString();// tài khoản người dùng
+            String mk = edittext_mk.getText().toString();// mật khẩu người dùng
 
+            // kiem tra empty
+            if (tk.equals("") || mk.equals("")) {
+                Toast.makeText(activity_login.this, "Vui lòng điền đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
-//                activity_login_asynctask login_asynctask = new activity_login_asynctask(activity_login.this, tk);
-//                login_asynctask.execute();
+            Handle.readData(new FirestoreCallBack() {
+                @Override
+                public void onCallBack(List<User> list) {
+                    if (Handle.usernameList.size() == 0) {
+                        Toast.makeText(activity_login.this, "Tài khoản hoặc mật khẩu sai!", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else {
+                        // kiểm tra password
+                        if (Handle.usernameList.get(0).checkPassword(mk)) {
+                            // thêm vào cookie => lần sau vào không cần đăng nhập nữa
+                            // khi người dùng đăng nhập vào thì ta lấy username của người dùng lưu lại
+                            // khi khởi động ứng dụng ta truy vấn vào sqlite này để xem nếu username tồn tại rồi thì
+                            // không cần đăng nhập vào thẳng trang dash_board cho người dùng
+                            File storagePath = getApplication().getFilesDir();
+                            String myDbPath = storagePath + "/" + "loginDb";
+                            sqlite = SQLiteDatabase.openDatabase(myDbPath, null, SQLiteDatabase.CREATE_IF_NECESSARY); // open db
 
-                Handle.readData(new FirestoreCallBack() {
-                    @Override
-                    public void onCallBack(List<User> list) {
-                        if (Handle.usernameList.size() == 0) {
+                            if (!Handle.tableExists(sqlite, "USER")) {
+                                // create table USER
+                                sqlite.execSQL("create table USER ("
+                                        + "username text PRIMARY KEY);");
+                            }
+
+                            sqlite.execSQL("insert into USER(username) values ('" + tk + "');");
+
+                            // chuyển sang giao diện chính
+                            Intent moveActivity = new Intent(activity_login.this, activity_dashboard.class);
+                            startActivity(moveActivity);
+                        } else {
                             Toast.makeText(activity_login.this, "Tài khoản hoặc mật khẩu sai!", Toast.LENGTH_SHORT).show();
                             return;
-                        } else {
-                            // kiểm tra password
-                            if (Handle.usernameList.get(0).checkPassword(mk)) {
-                                // thêm vào cookie => lần sau vào không cần đăng nhập nữa
-                                // khi người dùng đăng nhập vào thì ta lấy username của người dùng lưu lại
-                                // khi khởi động ứng dụng ta truy vấn vào sqlite này để xem nếu username tồn tại rồi thì
-                                // không cần đăng nhập vào thẳng trang dash_board cho người dùng
-                                File storagePath = getApplication().getFilesDir();
-                                String myDbPath = storagePath + "/" + "loginDb";
-                                sqlite = SQLiteDatabase.openDatabase(myDbPath, null, SQLiteDatabase.CREATE_IF_NECESSARY); // open db
-
-                                if (!Handle.tableExists(sqlite, "USER")) {
-                                    // create table USER
-                                    sqlite.execSQL("create table USER ("
-                                            + "username text PRIMARY KEY);");
-                                }
-
-                                sqlite.execSQL("insert into USER(username) values ('" + tk + "');");
-
-                                // chuyển sang giao diện chính
-                                Intent moveActivity = new Intent(activity_login.this, activity_dashboard.class);
-                                startActivity(moveActivity);
-                            } else {
-                                Toast.makeText(activity_login.this, "Tài khoản hoặc mật khẩu sai!", Toast.LENGTH_SHORT).show();
-                                return;
-                            }
                         }
                     }
-                }, tk);
-            }
-        });
+                }
+            }, tk);
+        }
 
-        // chuyển sang activity đăng ký
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent moveActivity = new Intent(activity_login.this, activity_register.class);
-                startActivity(moveActivity);
-            }
-        });
+        if (view.getId() == btn_google.getId()) {
+            SignInWithGoogle();
+        }
     }
 
     // lấy dữ liệu từ facebook => đưa dữ liệu lên firestore
@@ -205,14 +220,77 @@ public class activity_login extends Activity {
                         }
                     }
                 });
+
         Bundle parameters = new Bundle();
         parameters.putString("fields", "id,email,first_name, middle_name,last_name");
         request.setParameters(parameters);
         request.executeAsync();
     }
 
+    void SignInWithGoogle() {
+        Intent singInIntent = gsc.getSignInIntent();
+        startActivityForResult(singInIntent, 1000);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                // lấy thông tin từ gg
+                GoogleSignInAccount accountGG = GoogleSignIn.getLastSignedInAccount(this);
+
+                String email = accountGG.getEmail().toString();
+                String fullname = accountGG.getDisplayName().toString();
+                String username = accountGG.getId().toString();
+                User newUser = new User(username, fullname, email);
+                newUser.setImage(accountGG.getPhotoUrl().toString());
+
+                usersRef.whereEqualTo("username", username)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    boolean isHave = false;
+
+                                    for (DocumentSnapshot document : task.getResult()) {
+                                        isHave = true;
+                                    }
+
+                                    // chưa có thì tạo account mới
+                                    if (!isHave) {
+                                        usersRef.add(newUser);
+                                    }
+
+                                    // thêm vào cookie => lần sau vào không cần đăng nhập nữa
+                                    File storagePath = getApplication().getFilesDir();
+                                    String myDbPath = storagePath + "/" + "loginDb";
+                                    sqlite = SQLiteDatabase.openDatabase(myDbPath, null, SQLiteDatabase.CREATE_IF_NECESSARY); // open db
+
+                                    if (!Handle.tableExists(sqlite, "USER")) {
+                                        // create table USER
+                                        sqlite.execSQL("create table USER ("
+                                                + "username text PRIMARY KEY);");
+                                    }
+
+                                    sqlite.execSQL("insert into USER(username) values ('" + username + "');");
+
+                                    // chuyển sang giao diện dash_board
+                                    Intent moveActivity = new Intent(getApplicationContext(), activity_dashboard.class);
+                                    startActivity(moveActivity);
+                                }
+                            }
+                        });
+
+            } catch (Exception e) {
+                Toast.makeText(activity_login.this, "Thất bại!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/androidproject08/activity_register.java
+++ app/src/main/java/com/example/androidproject08/activity_register.java
@@ -19,9 +19,15 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -32,13 +38,15 @@ import org.json.JSONObject;
 import java.io.File;
 import java.util.List;
 
-public class activity_register extends Activity {
+public class activity_register extends Activity implements View.OnClickListener {
     // khai báo dữ liệu UI
     View btn_login;
     Button btn_register, btn_google_dk, btn_facebook_dk;
     EditText edittext_tk_dk, edittext_mk_dk, edittext_nhaplaimk;
 
-    // khai báo dữ liệu BE
+    // google
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     // facebook
     private CallbackManager mCallbackManager;
@@ -55,21 +63,17 @@ public class activity_register extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
         btn_login = (View) findViewById(R.id.rectangle_3);
+        btn_login.setOnClickListener(this);
         btn_register = (Button) findViewById(R.id.btn_dangky);
         edittext_tk_dk = (EditText) findViewById(R.id.edittext_tk_dk);
         edittext_mk_dk = (EditText) findViewById(R.id.edittext_mk_dk);
         edittext_nhaplaimk = (EditText) findViewById(R.id.edittext_nhaplaimk);
         btn_google_dk = (Button) findViewById(R.id.btn_google_dk);
-
-        // đăng ký bằng tài khoản google
-        btn_google_dk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // xử lý
-
-            }
-        });
+        btn_google_dk.setOnClickListener(this);
 
         // đăng nhập/ đăng ký bằng facebook
         mCallbackManager = CallbackManager.Factory.create();
@@ -140,14 +144,62 @@ public class activity_register extends Activity {
                         });
             }
         });
+    }
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent moveActivity = new Intent(activity_register.this, activity_login.class);
-                startActivity(moveActivity);
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == btn_login.getId()) {
+            Intent moveActivity = new Intent(activity_register.this, activity_login.class);
+            startActivity(moveActivity);
+        }
+
+        if (view.getId() == btn_google_dk.getId()) {
+            SignInWithGoogle();
+        }
+
+        if (view.getId() == btn_register.getId()) {
+            String tk = edittext_tk_dk.getText().toString();
+            String mk = edittext_mk_dk.getText().toString();
+            String mkAgain = edittext_nhaplaimk.getText().toString();
+
+            if (tk.equals("") || mk.equals("") || mkAgain.equals("")) {
+                Toast.makeText(activity_register.this, "Vui lòng điền đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                return;
             }
-        });
+
+            // kiểm tra mật khẩu nhập lại có khớp hay không
+            if (!mk.equals(mkAgain)) {
+                Toast.makeText(activity_register.this, "Mật khẩu nhập lại không khớp!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // check tài khoản đã tồn tại hay chưa
+            usersRef.whereEqualTo("username", tk)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                boolean isExisted = false;
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    isExisted = true;
+                                }
+                                if (isExisted) {
+                                    Toast.makeText(activity_register.this, "Tài khoản đã tồn tại!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    User newUser = new User(tk, mk);
+                                    usersRef.add(newUser);
+
+                                    // chuyển sang giao diện đăng nhập
+                                    Intent moveActivity = new Intent(getApplicationContext(), activity_login.class);
+                                    startActivity(moveActivity);
+                                }
+                            } else {
+                                Log.d("TAG", "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
+        }
     }
 
     // lấy dữ liệu từ facebook => đưa dữ liệu lên firestore
@@ -210,8 +262,70 @@ public class activity_register extends Activity {
         request.executeAsync();
     }
 
+    void SignInWithGoogle() {
+        Intent singInIntent = gsc.getSignInIntent();
+        startActivityForResult(singInIntent, 1000);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                // lấy thông tin từ gg
+                GoogleSignInAccount accountGG = GoogleSignIn.getLastSignedInAccount(this);
+
+                String email = accountGG.getEmail().toString();
+                String fullname = accountGG.getDisplayName().toString();
+                String username = accountGG.getId().toString();
+                User newUser = new User(username, fullname, email);
+                newUser.setImage(accountGG.getPhotoUrl().toString());
+
+                usersRef.whereEqualTo("username", username)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    boolean isHave = false;
+
+                                    for (DocumentSnapshot document : task.getResult()) {
+                                        isHave = true;
+                                    }
+
+                                    // chưa có thì tạo account mới
+                                    if (!isHave) {
+                                        usersRef.add(newUser);
+                                    }
+
+                                    // thêm vào cookie => lần sau vào không cần đăng nhập nữa
+                                    File storagePath = getApplication().getFilesDir();
+                                    String myDbPath = storagePath + "/" + "loginDb";
+                                    sqlite = SQLiteDatabase.openDatabase(myDbPath, null, SQLiteDatabase.CREATE_IF_NECESSARY); // open db
+
+                                    if (!Handle.tableExists(sqlite, "USER")) {
+                                        // create table USER
+                                        sqlite.execSQL("create table USER ("
+                                                + "username text PRIMARY KEY);");
+                                    }
+
+                                    sqlite.execSQL("insert into USER(username) values ('" + username + "');");
+
+                                    // chuyển sang giao diện dash_board
+                                    Intent moveActivity = new Intent(getApplicationContext(), activity_dashboard.class);
+                                    startActivity(moveActivity);
+                                }
+                            }
+                        });
+
+            } catch (Exception e) {
+                Toast.makeText(activity_register.this, "Thất bại!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
